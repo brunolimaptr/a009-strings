e9b5d8c4aa3ae96f7cb744a58eff8cb6d94b10ce
brunolimaptr <brunolima.ptr@gmail.com>
2022-08-31 13:31:07 -0300

    projeto2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,8 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaString2 = minhaString.trim()
+const minhaString3 = minhaString2. replace("________", "“sticioso”")
+
+console.log(`${minhaString.trim()}`)
+console.log(`Antes ${minhaString.length}`)
+console.log(`Depois ${minhaString2.length}`)
+console.log(minhaString3)
