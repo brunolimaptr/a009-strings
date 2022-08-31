f5083cd4de3eda0a6863d8ef79dbf4789950478d
brunolimaptr <brunolima.ptr@gmail.com>
2022-08-31 12:27:43 -0300

    projeto

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+
+let nome = prompt("Qual o seu nome?")
+let comida1 = prompt("Qual a sua comida favorita?")
+let comida2 = prompt("Qual a sua segunda comida favorita?")
+let comida3 = prompt("Qual a sua terceira comida favorita?")
+
+console.log(`Estas são as comidas favoritas do ${nome}:\n${comida1}\n${comida2}\n${comida3}`)
+
